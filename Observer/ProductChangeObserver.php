99303c2da781af012b673677d3b753b640fb76c7
Shreelatha G S <shreelatha.gs@houseology.com>
2018-05-17 12:13:29 +0100

    observer update

--- Observer/ProductChangeObserver.php
+++ Observer/ProductChangeObserver.php
@@ -4,36 +4,7 @@ namespace B2bapp\UpdateHandler\Observer;
 
 class ProductChangeObserver implements \Magento\Framework\Event\ObserverInterface
 {
-    public function detectProductChanges($observer)
-    {
-        /**
-         * @var $product Mage_Catalog_Model_Product
-         */
-        $product = $observer->getEvent()->getProduct();
-        if ($product->hasDataChanges()) {
-            try {
-                $model = \Magento\Framework\App\ObjectManager::getInstance()
-                    ->create('B2bapp\UpdateHandler\Model\UpdatedEntities');
-
-                $model->load($product->getSku(), 'sku');
-
-                $model->addData([
-                    "id" => $product->getId(),
-                    "entity" => 'product',
-                    "action" => 'change',
-                    "sku" => $product->getSku(),
-                    "updated_at" => (new \DateTime())->format(\Magento\Framework\Stdlib\DateTime::DATETIME_PHP_FORMAT),
-                    "status" => true
-                ]);
-                $model->save();
-            } catch (Exception $e) {
-                echo "error saving updated entties";
-            }
-        }
-        return $this;
-    }
-
-    public function detectProductDelete($observer)
+    public function detectProductChanges(\Magento\Framework\Event\Observer $observer)
     {
         /**
          * @var $product Mage_Catalog_Model_Product
@@ -45,28 +16,31 @@ class ProductChangeObserver implements \Magento\Framework\Event\ObserverInterfac
 
             $model->load($product->getSku(), 'sku');
 
+            $action = '';
+            if($observer->getEvent()->getName() == 'catalog_product_save_before') {
+                $action = 'change';
+            } else if($observer->getEvent()->getName() == 'catalog_product_delete_after') {
+                $action = 'delete';
+            }
+
             $model->addData([
                 "id" => $product->getId(),
                 "entity" => 'product',
-                "action" => 'delete',
+                "action" => $action,
                 "sku" => $product->getSku(),
                 "updated_at" => (new \DateTime())->format(\Magento\Framework\Stdlib\DateTime::DATETIME_PHP_FORMAT),
                 "status" => true
             ]);
             $model->save();
         } catch (Exception $e) {
-            echo "error saving updated entties";
+            echo "error saving updated entities";
         }
         return $this;
     }
 
     public function execute(\Magento\Framework\Event\Observer $observer)
     {
-        if($observer->getEvent()->getName() == 'catalog_product_save_before') {
-            $this->detectProductChanges($observer);
-        } else if($observer->getEvent()->getName() == 'catalog_product_delete_after') {
-            $this->detectProductDelete($observer);
-        }
+        $this->detectProductChanges($observer);
 
         return $this;
     }
